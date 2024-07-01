6f91fab2a741c18b3a2f7a003268ff20a7e12964
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-01 11:10:13 +0300

    Successfully managed to display questions and answers. Now adding logic and shuffling answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
+import QUESTIONS from "../questions.js";
 
 function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
 
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
-    
+  const activeQuestionIndex = userAnswers.length;
 
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
 
-  return <div>Quiz</div>;
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
