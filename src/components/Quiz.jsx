d7e10a84c9d5bd581b51c6f73906d705225e9b91
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-01 12:16:42 +0300

    Done with adding logic and sorting answers to shuffle

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +14,24 @@ function Quiz() {
     });
   };
 
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="Quiz is finished" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="questions">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
